6b5267124d6f8baae212a2abb372a552e129b2c9
Balaji-Somasale-B00886160 <balaji.somasale@dal.ca>
2023-01-28 13:48:21 -0400

    My FizzBuzz code

---
+++ Fizzbuzz_Balaji.py
@@ -0,0 +1,20 @@
+
+N=101
+
+def FizzBuzz_Balaji(N):
+    for item in range(1, N):
+        if (item%3==0 and item%5!=0):
+            print ("Fizz\n", end = "")
+            # This occurs when it is divisible by 3
+        elif(item%5==0 and item%3!=0):
+            print ("Buzz\n", end = "")
+            # This occurs  when it is divisible by 5
+        elif(item%15==0):
+            print ("FizzBuzz\n", end = "")
+            # This occurs when it is divisible by 3 and 5
+        else:
+            print (item, end = "")
+            print("\n", end = "")
+
+#N=input(int("Enter a number from 1 to 100"))
+FizzBuzz_Balaji(N)
